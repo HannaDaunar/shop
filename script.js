d972d9caaf36eafe3c8bccada2be987068ddc82a
HannaDaunar <hannadaunar@yandex.by>
2024-11-02 18:34:09 +0100

    оекорректировала js, html, css

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const items = [{
         title: "крем для век с коллагеном AHC Premier",
         description: "Высокопитательный ампульный крем для век с коллагеном AHC Premier Ampoule In Eye Cream Collagen T4 оказывает выраженное подтягивающее действие, разглаживает мелкие мимические морщинки, способствует осветлению тёмных кругов. Содержит комплекс пептидов, витамин С, пробиотики, масло макадамии и гиалуроновую кислоту.",
         button: 1,
+        favorite: false,
         volume: "30мл",
         price: 2150,
         discount: null,
@@ -12,6 +13,7 @@ const items = [{
         title: "Антивозрастная сыворотка для век с женьшенем Fraijour",
         description: "Антивозрастная сыворотка для век с женьшенем Fraijour Alchemic Ginsenoside Contour Eye Serum подтягивает и укрепляет кожу вокруг глаз, уменьшает выраженность морщин, разглаживает и повышает упругость. Сокращает припухлости и тёмные круги.",
         button: 1,
+        favorite: false,
         volume: "20мл",
         price: 1090,
         discount: 20,
@@ -22,6 +24,7 @@ const items = [{
         title: "Лифтинг-крем для век с пептидным комплексом Medi-Peel",
         description: "Лифтинг-крем для век с пептидным комплексом Medi-Peel Peptide Bor Tox Eye Cream эффективно разглаживает гусиные лапки и мимические морщины в области вокруг глаз, подтягивает и увлажняет. Предотвращает появление новых заломов, повышает упругость и уплотняет ткани.",
         button: 1,
+        favorite: false,
         volume: "40мл",
         price: 3800,
         discount: 50,
@@ -32,6 +35,7 @@ const items = [{
         title: "Крем для век многофункционального действия Mizon",
         description: "Крем для век многофункционального действия Mizon Only One Eye Cream For Face выполняет сразу несколько действий: интенсивно увлажняет и питает, способствует разглаживанию морщин, замедляет процессы старения и борется с тёмными кругами.",
         button: 1,
+        favorite: false,
         volume: "30мл",
         price: 780,
         discount: null,
@@ -42,6 +46,7 @@ const items = [{
         title: "Гель-крем для проблемной кожи с экстрактом красного лука IsNtree",
         description: "Гель-крем для проблемной кожи с экстрактом красного лука IsNtree Onion Newpair Gel Cream обладает мощным антибактериальным свойством, стимулирует заживление акне и мелких высыпаний. Высокая антиоксидантная активность средства блокирует разрушающее действие токсинов и укрепляет защитные функции эпидермиса.",
         button: 1,
+        favorite: false,
         volume: "50мл",
         price: 2072,
         discount: null,
@@ -52,6 +57,7 @@ const items = [{
         title: "Восстанавливающий крем с пробиотиками и церамидами Neogen",
         description: "Восстанавливающий крем с пробиотиками и церамидами Neogen Dermalogy Probiotics Relief Cream делает кожу более гладкой и упругой, замедляет процессы окисления, препятствует преждевременному старению, разглаживает рельеф.",
         button: 1,
+        favorite: false,
         volume: "50мл",
         price: 2610,
         discount: null,
@@ -62,6 +68,7 @@ const items = [{
         title: "Водостойкий солнцезащитный крем с алоэ Holika Holika",
         description: "Водостойкий солнцезащитный крем с алоэ Holika Holika Aloe Waterproof Sun Cream SPF 50+ PA ++++ надёжно защищает от ультрафиолетовых лучей типа UVA и UVB, предотвращает появление покраснения и ожогов, препятствует фотостарению и появлению пигментных пятен. Не оставляет белых следов, быстро впитывается, подходит для лица и тела.",
         button: 1,
+        favorite: false,
         volume: "70мл",
         price: 1590,
         discount: null,
@@ -72,6 +79,7 @@ const items = [{
         title: "Санскрин с берёзовым соком Round Lab",
         description: "Санскрин с берёзовым соком Round Lab Birch Juice Moisturizing Mild-Up Sunscreen SPF50+ PA++++ отлично защищает чувствительную кожу от ультрафиолета, предотвращает покраснение и появление ожогов, препятствует фотостарению. Не вызывает жирного блеска, снижает чувствительность к внешним раздражителям.",
         button: 1,
+        favorite: false,
         volume: "50мл",
         price: 2240,
         discount: null,
@@ -82,6 +90,7 @@ const items = [{
         title: "Глиняная маска для сужения пор с бобами маш beplain",
         description: "Глиняная маска с бобами маш beplain Mung Bean Pore Clay Mask проникает глубоко в поры и выводит из них всех загрязнения, растворяет комедоны и помогает в борьбе с чёрными точками. Регулирует выработку себума, сужает расширенные поры и осветляет сальные нити.",
         button: 1,
+        favorite: false,
         volume: "120мл",
         price: 2250,
         discount: null,
@@ -92,6 +101,7 @@ const items = [{
         title: "Ночная гидрогелевая маска с церамидами против сухости Biodance",
         description: "Ночная гидрогелевая маска с церамидами против сухости Biodance Hydro Cera-Nol Real Deep Mask интенсивно питает и увлажняет, заживляет сухую и повреждённую кожу, снижает чувствительность и укрепляет защитный гидролипидный барьер, смягчает огрубевшие и обветренные участки, запускает внутренние процессы регенерации и снимает раздражение. Восстанавливает за 3-4 часа",
         button: 1,
+        favorite: false,
         volume: "34г",
         price: 553,
         discount: null,
@@ -102,6 +112,7 @@ const items = [{
         title: "Гидрогелевые патчи с протеинами шёлка JMsolution",
         description: "Гидрогелевые патчи с протеинами шёлка JMsolution Silky Cocoon Home Esthetic Eye Patch оказывают лёгкое охлаждающее действие, разглаживают мелкие морщинки и уменьшают их выраженность, снимают отёки и следы усталости.",
         button: 1,
+        favorite: false,
         volume: "90г (60шт)",
         price: 1112,
         discount: null,
@@ -112,6 +123,7 @@ const items = [{
         title: "Очищающее гидрофильное масло с бобами маш beplain",
         description: "Очищающее гидрофильное масло с бобами маш beplain Mung Bean Cleansing Oil эффективно удаляет все загрязнения, макияж любой стойкости, тональные и солнцезащитные средства. Масло проникает глубоко в поры, очищать и сужать их, сокращает выраженность чёрных точек и сальных нитей.",
         button: 1,
+        favorite: false,
         volume: "200мл",
         price: 2650,
         discount: null,
@@ -122,6 +134,7 @@ const items = [{
         title: "Увлажняющий гель для умывания с церамидами Hyggee",
         description: "Увлажняющий гель для умывания Hyggee pH Hyaluron Gel Cleanser бережно устраняет все виды загрязнений, остатки макияжа и излишки себума. Имеет нейтральный pH и подходит для очищения самой чувствительной кожи, снимает покраснения, успокаивает воспаления и предотвращает появление новых.",
         button: 1,
+        favorite: false,
         volume: "200 мл",
         price: 530,
         discount: null,
@@ -132,6 +145,7 @@ const items = [{
         title: "Увлажняющий тоник с пептидами и гиалуроновой кислотой JMsolution",
         description: "Увлажняющий тоник с пептидами и гиалуроновой кислотой JMsolution Water Luminous SOS Ringer Toner XL моментально освежает и наполняет влагой, делает кожу нежной, мягкой и упругой, предотвращает сухость и борется с чувством стянутости. Готовит кожу к дальнейшему уходу, нормализует баланс pH, поддерживает эластичность и гладкость.",
         button: 1,
+        favorite: false,
         volume: "600мл",
         price: 1490,
         discount: null,
@@ -142,6 +156,7 @@ const items = [{
         title: "Мультивитаминный тоник для тусклой кожи Manyo",
         description: "Мультивитаминный тонер для тусклой кожи Manyo Galac Whitening Vita Toner в кратчайшие сроки наполняет кожу влагой, возвращает тонус и здоровое сияние, улучшает цвет кожи, выравнивает тон и способствует осветлению пигментных пятен.",
         button: 1,
+        favorite: false,
         volume: "210мл",
         price: 2710,
         discount: null,
@@ -201,13 +216,21 @@ function prepareShopItem(shopItem) {
     for (let i = 0; i < button; i++) {
         const heart = document.createElement("i");
         heart.classList.add("fa-regular", "fa-heart");
+        if (shopItem.favorite) {
+            heart.classList.add("active");
+        }
         heart.addEventListener('click', function() {
             if (itemsFavourites.includes(shopItem)) {
                 heart.classList.remove('active');
-                itemsFavourites.splice(itemsFavourites[heart.closest(".shop-item")], 1);
+                shopItem.favorite = false;
+                const el = itemsFavourites.find((element) => element === shopItem);
+                const index = itemsFavourites.indexOf(el);
+                itemsFavourites.splice(index, 1);
             } else {
                 heart.classList.add('active');
                 itemsFavourites.push(shopItem);
+                shopItem.favorite = true;
+
             }
         })
         buttonContainer.append(heart);
@@ -224,15 +247,14 @@ function prepareShopItem(shopItem) {
 
 const btnFavourites = document.querySelector(".favourites");
 btnFavourites.addEventListener("click", function() {
-    currentState = itemsFavourites;
-    renderItems(currentState);
+    renderItems(itemsFavourites);
 })
 
 const buttonSale = document.querySelector(".btn-sale");
 buttonSale.addEventListener("click", function() {
-    currentState = items.filter((el) =>
+    const itemsOnSale = items.filter((el) =>
         el.discount > 0);
-    renderItems(currentState);
+    renderItems(itemsOnSale);
 })
 
 
@@ -282,3 +304,8 @@ sortControl.addEventListener("change", (event) => {
     renderItems(currentState);
 
 });
+const btnProducts = document.getElementById('products');
+btnProducts.addEventListener("click", function(e) {
+    e.preventDefault();
+    renderItems(currentState);
+})
